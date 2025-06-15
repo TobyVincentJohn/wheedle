5dcbb742941bc62302bbd8ebd6ce1a210ce5e65b
TobyVincentJohn <tobyvincentjohn@gmail.com>
2025-06-15 12:54:49 +0530

    Updated main.tsx

--- src/devvit/main.tsx
+++ src/devvit/main.tsx
@@ -4,6 +4,7 @@ import { Devvit, Post } from '@devvit/public-api';
 import '../server/index';
 import { defineConfig } from '@devvit/server';
 import { postConfigNew } from '../server/core/post';
+
 Devvit.addSettings([
   {
     name: 'open-ai-api-key',
